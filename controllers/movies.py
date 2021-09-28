0beaec40ed083e16cc56df598dca5a6d736f2e4f
“ELOBO5” <elicialobo@hotmail.com>
2021-09-28 16:29:02 +0100

    add to movies list

--- controllers/movies.py
+++ controllers/movies.py
@@ -1,5 +1,9 @@
 movies = [
-{"id": 1, "movie_name": "Dune", "release_date": 2021, "director": "Denis Villeneuve"}
+{"id": 1, "movie_name": "Dune", "release_date": 2021, "director": "Denis Villeneuve"},
+{"id": 2, "movie_name": "Tenet", "release_date": 2020, "director": "Christopher Nolan"},
+{"id": 3, "movie_name": "A Quiet Place Part II", "release_date": 2020, "director": "John Krasinski"},
+{"id": 4, "movie_name": "Wonder Woman 1984", "release_date": 2020, "director": "Patty Jenkins"},
+{"id": 5, "movie_name": "The King", "release_date": 2019, "director": "David Michôd"}
 ]
 
 def all():
