57ae1fc1abef915bf1aa0232b6c4608a0783eb32
“ELOBO5” <elicialobo@hotmail.com>
2021-09-28 11:57:11 +0100

    create model

---
+++ api/user.py
@@ -0,0 +1,27 @@
+class User():
+    all = []
+
+    def __init__(self, data):
+        self._username = data['username']
+        self._age = data['age']
+        self._height = data['height']
+        self._save()
+
+    def _save(self):
+        self.all.append(self)
+
+    @property
+    def username(self):
+        return self._username
+    
+    @property
+    def description(self):
+        return self._age
+
+    @property
+    def height(self):
+        return self._height
+    
+    @classmethod
+    def find_by_input(cls, user_input):
+        return cls.all[int(user_input)-1]
