a26ee8661cab4ae2f47d00ab76abc05db5ad26f0
mariuszlas <mariusz.las@outlook.com>
2021-09-28 12:06:34 +0100

    removed docker-compose

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
-from werkzeug import exceptions 
+from werkzeug import exceptions
 from flask_cors import CORS
-
+from user import User
 
 app = Flask(__name__)
 CORS(app)
@@ -11,16 +11,13 @@ def hello_world():
     return 'hello world'
 
 @app.route('/users', methods = ['GET', 'POST'])
-def giriffle_handler():
+def user_handler():
     if request.method == 'GET':
         return jsonify(User.query.all())
     elif request.method == 'POST':
         data = request.json
-        db.session.add(data['username'])
-        db.session.add(data['age'])
-        db.session.add(data['height'])
-        db.session.commit()
-        return 'your information has been added', 201
+        User.create(data)
+        return 'user has been added', 201
   
 @app.route('/giriffle/<int:giriffle_id>')
 def indexed_giriffle(giriffle_id):
