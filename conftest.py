46dbf18847d2b0627bd7aac53b019905ea68e11e
“ELOBO5” <elicialobo@hotmail.com>
2021-09-29 09:33:50 +0100

    fix tests

--- conftest.py
+++ conftest.py
@@ -6,6 +6,6 @@ from controllers import movies
 @pytest.fixture
 def api(monkeypatch):
     test_movies = [{'id': 1, 'movie_name': 'Terminator', 'release_date': 1984, 'director': 'James Cameron'}]
-    monkeypatch.setattr(app, "movies", test_movies)
+    monkeypatch.setattr(movies, "movies", test_movies)
     api = app.app.test_client()
     return api
--- test_app.py
+++ test_app.py
@@ -6,28 +6,34 @@ class TestAPICase():
         assert res.status == '200 OK'
         assert res.json['message'] == "Welcome to the movies API!"
 
-    def test_get_movies(self, api):
+    def test_all(self, api):
         res = api.get('/movies')
         assert res.status == '200 OK'
-        assert len(res.json) == 5
+        assert len(res.json) == 1
 
-    def test_get_movie_by_id(self, api):
-        res = api.get('/api/movies/1')
+    def test_find_by_id(self, api):
+        res = api.get('/movies/1')
         assert res.status == '200 OK'
-        assert res.json['movie_name'] == 'Dune'
-
-    def test_get_movies_error(self, api):
-        res = api.get('/api/movies/10')
-        assert res.status == '400 BAD REQUEST'
-        assert "movie with id 10" in res.json['message']
+        assert res.json['movie_name'] == 'Terminator'
 
     def test_post_movies(self, api):
         mock_data = json.dumps({'movie_name': 'Stand by me'})
         mock_headers = {'Content-Type': 'application/json'}
-        res = api.post('/api/movies', data=mock_data, headers=mock_headers)
-        assert res.json['id'] == 5
+        res = api.post('/movies', data=mock_data, headers=mock_headers)
+        assert res.json['id'] == 2
+    
+    def test_put_movie(self, api):
+        mock_data = json.dumps({'release_date': 2000})
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.put('/movies/1', data=mock_data, headers=mock_headers)
+        assert res.json['id'] == 1
+        assert res.json['release_date'] == 2000
+
+    def test_delete_movie(self, api):
+        res = api.delete('/movies/1')
+        assert res.status == '204 NO CONTENT'
 
     def test_not_found(self, api):
         res = api.get('/non')
         assert res.status == '404 NOT FOUND'
-        assert 'Oops!' in res.json['message']
+        assert 'Not Found' in res.json['message']
